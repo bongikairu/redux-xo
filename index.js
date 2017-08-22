e93a37e24f72f36674f1c311096f1d3cae17ba80
Sutas Nakasawek <sutas@arterex.com>
2017-08-22 17:58:47 +0700

    add babel node

--- index.js
+++ index.js
@@ -49,3 +49,81 @@ store.dispatch(action_place("1_1"));
 debug();
 store.dispatch(action_place("0_2"));
 debug();
+//
+// let x = [
+//     {
+//         id: 1,
+//         name: "x"
+//     },
+//     {
+//         id: 2,
+//         name: "y"
+//     }
+// ];
+//
+// console.log(x);
+// x = x.map((l) => {
+//     if (l.id === 2) {
+//         return {
+//             id: 2,
+//             name: "new"
+//         }
+//     } else {
+//         return l;
+//     }
+// });
+// console.log(x);
+//
+// console.log(x.filter(x => x.name === 'new'));
+//
+// console.log(x.filter(x => {
+//     return x.name === 'new'
+// }));
+//
+// const z = (x) => {
+//     return {
+//         'a': 1
+//     }
+// };
+//
+// const z2 = (x) => {
+//     'a': 1
+// }
+//
+// const y = (x) => ({
+//     'a': 1
+// });
+//
+// function x(a, b) {
+//     console.log(this, a, b);
+// }
+//
+// x();
+//
+// const x2 = x.bind("123");
+//
+// x2();
+//
+//
+// x.apply("123", [4, 5]);
+//
+// x(...[4, 5]);
+//
+// function connect_server(url, id, password) {
+//     console.log(url, id, password)
+// }
+//
+// let param = [
+//     "http://asdsdsa.dasd.das/",
+//     "yyy",
+//     "zzz"
+// ];
+//
+// connect_server(param[0],param[1],param[2]);
+// connect_server(...param);
+//
+// function log(...args) {
+//     console.log(args);
+// }
+//
+// log(1,2,3,4,5);
